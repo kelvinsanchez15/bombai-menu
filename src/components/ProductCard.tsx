c448b87d16566b1b5f542f7f4c732c0ed6868f45
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2021-04-17 14:03:12 -0400

    fix layout issue

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -22,52 +22,64 @@ export default function ProductCard({
       backgroundColor="whiteAlpha.900"
       boxShadow="md"
     >
-      <Box height="full">
-        <Image
-          borderRadius="3xl"
-          src={product.image}
-          alt={product.title}
-          dimensions={[400, 300]}
-          objectFit="cover"
-        />
-      </Box>
+      <Image
+        borderRadius="3xl"
+        src={product.image}
+        alt={product.title}
+        dimensions={[400, 300]}
+        objectFit="cover"
+      />
 
-      <Stack padding={5} height="full" justifyContent="space-between">
-        <Box minH={100}>
-          <Heading size="lg">{product.title}</Heading>
-          <Text color="GrayText">{product.description}</Text>
-        </Box>
-        <Box display="flex" justifyContent="space-between">
-          <Text fontSize="md" fontWeight="bold">
-            Precio
-          </Text>
-          <Text fontSize="md" fontWeight="bold">
-            Cantidad
-          </Text>
-        </Box>
-        <Box display="flex" justifyContent="space-between" alignItems="center">
-          <Heading size="lg" as="span" flexBasis={1}>
-            {parseCurrency(product.price)}
+      <Stack
+        padding={5}
+        height={{ base: 'auto', sm: 60 }}
+        justifyContent="space-between"
+      >
+        <Box>
+          <Heading size="lg" mb={1}>
+            {product.title}
           </Heading>
-
-          <Box display="flex" alignItems="center">
-            <Button
-              colorScheme="blackAlpha"
-              borderRadius="full"
-              onClick={() => handleDecreaseProductQuantity(product.id)}
-            >
-              -
-            </Button>
-            <Text fontWeight="bold" paddingX={3}>
-              {product.quantity}
+          <Text lineHeight="short" color="GrayText">
+            {product.description}
+          </Text>
+        </Box>
+        <Box>
+          <Box display="flex" justifyContent="space-between">
+            <Text fontSize="md" fontWeight="bold">
+              Precio
             </Text>
-            <Button
-              colorScheme="red"
-              borderRadius="full"
-              onClick={() => handleIncreaseProductQuantity(product.id)}
-            >
-              +
-            </Button>
+            <Text fontSize="md" fontWeight="bold">
+              Cantidad
+            </Text>
+          </Box>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Heading size="lg" as="span" flexBasis={1}>
+              {parseCurrency(product.price)}
+            </Heading>
+
+            <Box display="flex" alignItems="center">
+              <Button
+                colorScheme="blackAlpha"
+                borderRadius="full"
+                onClick={() => handleDecreaseProductQuantity(product.id)}
+              >
+                -
+              </Button>
+              <Text fontWeight="bold" paddingX={3}>
+                {product.quantity}
+              </Text>
+              <Button
+                colorScheme="red"
+                borderRadius="full"
+                onClick={() => handleIncreaseProductQuantity(product.id)}
+              >
+                +
+              </Button>
+            </Box>
           </Box>
         </Box>
       </Stack>
