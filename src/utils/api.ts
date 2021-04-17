0f6d4e9a2e7f2164507199110cf8d3f4b6a89e20
Kelvin Sanchez <kelvinsanchez15@gmail.com>
2021-04-17 12:03:06 -0400

    add bombai db fetch csv fetch url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,7 +4,7 @@ import { Product } from '../types';
 export default {
   getProducts: async () => {
     const GOOGLE_SHEET_CSV_URL =
-      'https://docs.google.com/spreadsheets/d/e/2PACX-1vSVytybSAiUSowMR3VFqH98PpTkOqbh5eYGZYeGjOM62V510K7VEC-jrbhNfJFDeQ32XHd4br_3x_T5/pub?output=csv';
+      'https://docs.google.com/spreadsheets/d/e/2PACX-1vTaN9_CVhJZkMjQqIyc-r201MFonXpLio9njJ5i3i-qD_OGLTOS-UvnLjkckuU6SONa6Ipai0pE5WzT/pub?output=csv';
     try {
       const res = await fetch(GOOGLE_SHEET_CSV_URL, {
         headers: { 'content-type': 'text/csv;charset=UTF-8' },
